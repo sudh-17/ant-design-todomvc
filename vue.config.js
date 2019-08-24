09c582a6259695cc8dd5a2498e9da4789ae2dfad
sudh-17 <395338095@qq.com>
2019-08-24 18:03:25 +0800

    change path of build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/ant-design-todomvc/dist/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/ant-design-todomvc' : '/'
 }
